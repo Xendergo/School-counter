2dd844c093f3bf8f14955ad05f1d9df4442a8ab3
F4Tornado <h.rovnyak@gmail.com>
2020-05-05 11:57:01 -0400

    Removing p5.js and fixing the download button
    Previously, the download button would save an image the size of your screen which would mess up the drawer. Now it saves to a constant 1080x1080

--- sketch.js
+++ sketch.js
@@ -13,18 +13,21 @@ const monthNames = ["January", "February", "March", "April", "May", "June",
   "July", "August", "September", "October", "November", "December"
 ];
 
+const c = document.getElementById("c");
+const draw = c.getContext("2d")
+
 const palette = {
   line: () => {
-    return color(c, 100, 100)
+    return `hsl(${color}, 100%, 50%)`
   },
   background: () => {
-    return color(0, 0, 100, 0);
+    return `hsl(0, 0%, 100%, 0)`;
   }
 }
 let points = 200;
 let multiplier = 0;
 let cs = [125, 276];
-let c = cs[0];
+let color = cs[0]
 let cnv;
 let pm = 0;
 const mod = (x, n) => (x % n + n) % n;
@@ -35,9 +38,8 @@ let radios = document.getElementsByName("measure");
 let date = new Date()
 
 function setup() {
-  colorMode(HSB)
-  cnv = createCanvas(windowHeight, windowHeight);
-  textFont("Ubuntu");
+  c.width = window.innerHeight * 0.85;
+  c.height = window.innerHeight * 0.85;
   updater = new Worker("./updater.js");
   updater.onmessage = function (e) {
     if (e.data == "Set Favicon") {
@@ -52,16 +54,18 @@ function setup() {
       measurement = radios[i].value;
     }
   }
+
+  drawLoop();
 }
 
-function draw() {
+function drawLoop() {
+  setTimeout(drawLoop, 1000 / 60);
   for (let i = 0; i < radios.length; i++) {
     if (radios[i].checked) {
       measurement = radios[i].value;
     }
   }
 
-  select("#tableSettings").position(0, 0);
   const monthStart = getNumValue("monthStart") - 1;
   const dayStart = getNumValue("dayStart");
   const hourStart = getNumValue("hourStart");
@@ -75,6 +79,7 @@ function draw() {
   const preview = document.getElementById("preview").checked;
   let dateStart;
   let dateEnd;
+
   if (measurement == "year") {
     dateStart = new Date(yr1, monthStart, dayStart, hourStart, minuteStart);
     dateEnd = new Date(yr2, monthEnd, dayEnd, hourEnd, minuteEnd);
@@ -97,79 +102,80 @@ function draw() {
   if (nextPercentagePoint.getHours() > 12) {
     morning = "PM"
   }
+
+
   document.getElementById("nextPercentagePoint").innerHTML = `% happening at: ${Math.round(mod((nextPercentagePoint.getHours() - 0.1), 12))}:${("0" + nextPercentagePoint.getMinutes()).slice(-2)} ${morning}, ${monthNames[nextPercentagePoint.getMonth()]} ${nextPercentagePoint.getUTCDate()} `;
   multiplier = (now / (dateEndMillis - dateStartMillis)) * 100;
+
   if (Math.floor(pm / notifyMod) * notifyMod !== Math.floor(multiplier / notifyMod) * notifyMod && pm !== 0) {
     notify(`School is ${Math.floor(multiplier * 10000) / 10000}% over!`);
-    console.log("Notify")
   }
+
   pm = multiplier;
   document.getElementById("percent").innerHTML = multiplier + "%";
-  if (select("#roundTable").elt.checked) {
-    select("#roundValue").elt.hidden = false
-    multiplier = Math.round(multiplier / select("#roundValue").value()) * select("#roundValue").value();
+  if (document.getElementById("roundTable").checked) {
+    document.getElementById("roundValue").hidden = false;
+    let roundValue = getNumValue("roundValue");
+    multiplier = Math.round(multiplier / roundValue * roundValue);
   } else {
-    select("#roundValue").elt.hidden = true
+    document.getElementById("roundValue").hidden = true;
   }
-  select("#multiplier").elt.innerHTML = `x${multiplier}`
-  // console.log(multiplier);
-  select("#percent").elt.width = windowWidth / 2;
-  // background(palette.background())
+  document.getElementById("multiplier").innerHTML = `x${multiplier}`
+  document.getElementById("percent").width = c.windowWidth / 2;
 
   if (preview) {
     multiplier = getNumValue("point");
   }
 
-  clear()
-  translate(width / 2, height / 2.5);
-  textSize(width / (410 / 11));
-  for (let i = 0; i < points; i++) {
-    fill(palette.line());
-    c = (i / points) * (cs[1] - cs[0]) + cs[0]
-    let p = polarToCart([i / points * PI * 2, width * 0.4]);
-    let p2 = polarToCart([i / points * PI * 2 * multiplier, width * 0.4]);
-    stroke(palette.line());
-    strokeWeight(width / (1640 / 3))
-    line(p[0], p[1], p2[0], p2[1]);
-    noStroke()
-    ellipse(p[0], p[1], width / 256, height / 256);
-  }
-  cs[1] = sin(multiplier * 50) * 100 + 200;
+  drawCircle();
+
+  cs[1] = Math.sin(multiplier * 50) * 100 + 200;
   cs[0] = cs[1] - 100;
-  if (millis() < 1000) {
-    setFavicon()
+  if (performance.now() < 1000) {
+    setFavicon();
   }
 }
 
 function polarToCart(v) {
-  return [v[1] * cos(v[0]), v[1] * sin(v[0])];
+  return [v[1] * Math.cos(v[0]), v[1] * Math.sin(v[0])];
+}
+
+function drawCircle() {
+  draw.save();
+  draw.clearRect(0, 0, c.width, c.height);
+  draw.translate(c.width / 2, c.height / 2);
+  for (let i = 0; i < points; i++) {
+    draw.fillStyle = palette.line();
+    color = (i / points) * (cs[1] - cs[0]) + cs[0]
+    let p = polarToCart([i / points * Math.PI * 2, c.width * 0.48]);
+    let p2 = polarToCart([i / points * Math.PI * 2 * multiplier, c.width * 0.48]);
+    draw.strokeStyle = palette.line();
+    draw.lineWidth = c.width / (1640 / 3);
+    draw.beginPath();
+    draw.moveTo(p[0], p[1]);
+    draw.lineTo(p2[0], p2[1]);
+    draw.stroke();
+
+    draw.beginPath();
+    draw.arc(p[0], p[1], c.height / 512, 0, Math.PI * 2);
+    draw.fill();
+  }
+  draw.restore();
 }
 
 function savey() {
-  let temp = cnv.position();
   // console.log(temp)
-  resizeCanvas(windowWidth, windowHeight);
-  cnv.position(0, 0);
-  clear();
-  translate(-width / 2, -height / 2.5);
-  translate(width / 2, height / 2)
-  textSize(width / (410 / 11));
-  for (let i = 0; i < points; i++) {
-    fill(palette.line());
-    c = (i / points) * (cs[1] - cs[0]) + cs[0]
-    let p = polarToCart([i / points * PI * 2, height * 0.45]);
-    let p2 = polarToCart([i / points * PI * 2 * multiplier, height * 0.45]);
-    stroke(palette.line());
-    strokeWeight(height / (1640 / 2.5))
-    line(p[0], p[1], p2[0], p2[1]);
-    noStroke()
-    ellipse(p[0], p[1], height / 256, height / 256);
-  }
-  saveCanvas(cnv, `x${multiplier}`, "png")
-  translate(-width / 2, -height / 2);
-  translate(width / 2, height / 2.5);
-  resizeCanvas(windowHeight, windowHeight);
-  cnv.position(temp.x, temp.y)
+  c.width = 1080;
+  c.height = 1080;
+
+  drawCircle();
+
+  document.getElementById("downloadLink").href = c.toDataURL();
+  document.getElementById("downloadLink").download = `x${multiplier}.png`;
+  document.getElementById("downloadLink").click();
+
+  c.width = window.innerHeight;
+  c.height = window.innerHeight;
 }
 
 function notify(msg) {
@@ -204,9 +210,11 @@ const setFavicon = () => {
   let link = document.getElementById("favicon");
   link.type = 'image/x-icon';
   link.rel = 'shortcut icon';
-  link.href = canvas.toDataURL("image/x-icon");
+  link.href = c.toDataURL("image/x-icon");
 }
 
 Number.prototype.mod = function (n) {
   return ((this % n) + n) % n;
 };
+
+setup();
